bf3c248bbf3d0b7dc4f4083bb784c1e9f9c56c90
MelihUnal60 <111892158+MelihUnal60@users.noreply.github.com>
2023-06-03 07:22:46 +0300

    Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import {Route, Routes} from "react-router-dom"
 import Home from "./pages/Home/Home"
 import About from "./pages/About/About"
+import Appointment from "./pages/Appointment/Appointment"
+import Contact from "./pages/Contact/Contact"
+import Treatments from "./pages/Treatments/Treatments"
+import Error from "./pages/Error/Error"
 
 function App() {
   return (
@@ -10,7 +14,12 @@ function App() {
         <div className="Main">
           <Routes>
             <Route path = "/" element={<Home />} />
-            <Route path = "" element={<Home />} />
+            <Route path = "/about" element={<About />} />
+            <Route path = "/appointment" element={<Appointment />} />
+            <Route path = "/contact" element={<Contact />} />
+            <Route path = "/treatments" element={<Treatments />} />
+            <Route path = "/error" element={<Error />} />
+
           </Routes>
         </div>
         {/* footer */}
