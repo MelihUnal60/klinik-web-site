d47b32b0e5e705a3c8d5c39cac9827c9a7932f5e
MelihUnal60 <111892158+MelihUnal60@users.noreply.github.com>
2023-06-05 22:55:19 +0300

    Sass features

--- src/Components/Nav/Naav.js
+++ src/Components/Nav/Naav.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Container,Navbar,Nav } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import {AiOutlineAlert} from 'react-icons/ai'
 
 const Naav=() => {
   return (
