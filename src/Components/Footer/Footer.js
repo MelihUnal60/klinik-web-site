4a16007d331cc9fa031a3c40d2d1d137567c74f7
MelihUnal60 <111892158+MelihUnal60@users.noreply.github.com>
2023-06-08 21:01:49 +0300

    Footer edits

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import styles from'./Footer.module.scss';
 
+
 function Footer() {
   return (
     <div className={styles.footer}>
 
         <div className='container d-flex'>
-            <div className='col-3'>
-                <h4 className='mt-5'>BİZE ULAŞIN</h4>
+            <div className='col-3 border-right-2px'>
+                <h4 className='mt-5 mb-3'>BİZE ULAŞIN</h4>
+                <h4 className='row justify-content-center mb-3'>
+                 0212 503 00 05
+                </h4>
+                <h4 className='row justify-content-center mb-3'>
+                 Online Randevu
+                </h4>
+                <h4 className='row justify-content-center mb-3'>
+                 caliskandent@gmail.com
+                </h4>
+                <h4 className='row justify-content-center'>
+                 iconlar gelecek
+                </h4>
             </div>
             <div className='col-3'>
                 <h4 className='mt-5'>NASIL GİDİLİR?</h4>
