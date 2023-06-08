a2b2734372cff365641ac7ad00fd7dd0ce4b281f
MelihUnal60 <111892158+MelihUnal60@users.noreply.github.com>
2023-06-08 13:58:58 +0300

    Footer Features

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import './Footer.module.scss';
+import styles from'./Footer.module.scss';
 
 function Footer() {
   return (
-    <div>
+    <div className={styles.footer}>
 
-        <h1>Buraya Footer Gelecek</h1>
+        <div className='container d-flex'>
+            <div className='col-3'>
+                <h4 className='mt-5'>BİZE ULAŞIN</h4>
+            </div>
+            <div className='col-3'>
+                <h4 className='mt-5'>NASIL GİDİLİR?</h4>
+            </div>
+            <div className='col-3'>
+                <h4 className='mt-5'>LOKASYONUMUZ</h4>
+            </div>
+            <div className='col-3'>
+                <h4 className='mt-5'>SİZİ ARAYALIM</h4>
+            </div>
+        </div>   
+
     </div>
   )
 }
---
+++ src/Components/TreatmentList/TreatmentList.js
@@ -0,0 +1,1 @@
+import './TreatmentList.module.scss';
