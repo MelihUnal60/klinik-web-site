9494a7f4aef3007521a813931664039a6153b42e
MelihUnal60 <111892158+MelihUnal60@users.noreply.github.com>
2023-06-09 06:05:07 +0300

    Footer icons & buttons

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,39 +1,37 @@
 import React from 'react';
-import styles from'./Footer.module.scss';
+import styles from './Footer.module.scss';
+import {BsTelephoneInboundFill} from 'react-icons/bs';
+import {BsFacebook} from 'react-icons/bs';
+import {RiInstagramFill} from 'react-icons/ri';
 
 
 function Footer() {
-  return (
-    <div className={styles.footer}>
+    return (
+        <div className={styles.footer}>
 
-        <div className='container d-flex'>
-            <div className='col-3 border-right-2px'>
-                <h4 className='mt-5 mb-3'>BİZE ULAŞIN</h4>
-                <h4 className='row justify-content-center mb-3'>
-                 0212 503 00 05
-                </h4>
-                <h4 className='row justify-content-center mb-3'>
-                 Online Randevu
-                </h4>
-                <h4 className='row justify-content-center mb-3'>
-                 caliskandent@gmail.com
-                </h4>
-                <h4 className='row justify-content-center'>
-                 iconlar gelecek
-                </h4>
+            <div className='container d-flex'>
+                <div className='col-3 border-right-2px'>
+                    <h4 className='mt-5 mb-3'>BİZE ULAŞIN</h4>
+                    <button class="btn btn-outline-secondary w-100 mb-3" type="button"><BsTelephoneInboundFill />0212 503 00 05</button>
+                    <button class="btn btn-outline-secondary w-100 mb-3" type="button">Online Randevu</button>
+                    <button class="btn btn-outline-secondary w-100 mb-3" type="button">caliskandent@gmail.com</button>
+                    <button class="btn btn-outline-secondary w-100 mb-3" type="button">
+                        <BsFacebook />
+                        <RiInstagramFill />
+                        </button>
+                </div>
+                <div className='col-3'>
+                    <h4 className='mt-5'>NASIL GİDİLİR?</h4>
+                </div>
+                <div className='col-3'>
+                    <h4 className='mt-5'>LOKASYONUMUZ</h4>
+                </div>
+                <div className='col-3'>
+                    <h4 className='mt-5'>SİZİ ARAYALIM</h4>
+                </div>
             </div>
-            <div className='col-3'>
-                <h4 className='mt-5'>NASIL GİDİLİR?</h4>
-            </div>
-            <div className='col-3'>
-                <h4 className='mt-5'>LOKASYONUMUZ</h4>
-            </div>
-            <div className='col-3'>
-                <h4 className='mt-5'>SİZİ ARAYALIM</h4>
-            </div>
-        </div>   
 
-    </div>
-  )
+        </div>
+    )
 }
 export default Footer;
--- src/Components/Nav/Naav.js
+++ src/Components/Nav/Naav.js
@@ -4,10 +4,75 @@ import { NavLink } from 'react-router-dom';
 import {AiOutlineAlert} from 'react-icons/ai'
 import styles from './Naav.module.scss';
 
-const Naav=() => {
-  return (
-    <h1>Buraya Navbar Gelecek</h1>
-  )
+const Naav = () => {
+    const menus = [
+        {
+            name: "Home",
+            id: 1,
+            path: "/"
+        },
+        {
+            name: "About Us",
+            id: 2,
+            path: "/about"
+        },
+        {
+            name: "Appoitment",
+            id: 3,
+            path: "/appointment"
+        },
+        {
+            name: "Contact",
+            id: 4,
+            path: "/contact"
+        },
+        {
+            name: "Treatments",
+            id: 5,
+            path: "/treatments"
+        },
+        {
+            name: "Error",
+            id: 6,
+            path: "/*"
+        },
+    ]
+    return (
+        <div>
+            <Navbar expand="lg" className={`${styles.navBar} fixed-top`}>
+                <Container>
+                    <Navbar.Brand>
+                        <NavLink to="/" className={`${styles.navLink} text-uppercase`}>
+
+                           Diş Kliniği 
+
+                        </NavLink>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScrooll" />
+                    <Navbar.Collapse id="navbarScrooll">
+                        <Nav className="ms-auto my-2 my-lg-0"
+                            style={{ maxHeight: "100px" }}
+                            navbarScrooll>
+
+                            {
+                                menus.map((menu) => (
+                                    <NavLink
+                                        key={menu.id}
+                                        to={menu.path}
+                                        className={`${styles.navLink} ${styles.menuLink}`}>
+                                        {menu.name}
+                                    </NavLink>
+                                ))
+                            }
+
+                       </Nav>
+                    </Navbar.Collapse>
+
+                </Container>
+            </Navbar>
+
+        </div >
+    );
 }
 
 export default Naav;
