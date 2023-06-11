e78af5aef89ba6ec3686c41f6e5f165caa030c9d
MelihUnal60 <111892158+MelihUnal60@users.noreply.github.com>
2023-06-11 15:09:59 +0300

    Main content hover

--- src/Components/MainContent/MainContent.js
+++ src/Components/MainContent/MainContent.js
@@ -15,7 +15,7 @@ function MainContent() {
                 />
                 <div className={styles.mainBox}>
                 <h1 className={styles.mainText}>Çalışkan Dental Kliniğe Hoş Geldiniz!</h1>
-                <h5 className={styles.mainText2}>34 yıllık tecrubemiz ve uzman kadromuz ile dişinizin sağlığı ve estetiği için yanınızdayız;</h5>
+                <h6 className={styles.mainText2}>34 yıllık tecrubemiz ve uzman kadromuz ile dişinizin sağlığı ve estetiği için yanınızdayız;</h6>
                 <h6 className={styles.mainText2}><FaTooth /> Çalışkan Dent olarak sağlıkla ve güvenle gülümsemeniz için, kalıcı ve ekonomik çözümlerle hizmetinizdeyiz.</h6>
                 <h6 className={styles.mainText2}><FaTooth /> Tedavi teknolojilerindeki gelişmeleri sürekli takip ederek en çağdaş malzemeler, en yeni cihazlar ile konforunuzu arttırmaya özen gösteriyoruz.</h6>
                 <h6 className={styles.mainText2}><FaTooth /> Implantoloji, endodonti(kanal tedavisi), zirkonyum, çene cerrahisi, bleacthing(diş beyazlatma),kompozit-estetik dolgu uygulama ve operasyonlarında alanında uzman hekimlerimiz ile dişlerinizin sağlığına ve estetiğine katkı sunuyoruz.</h6>
