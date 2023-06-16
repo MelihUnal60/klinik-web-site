12634857484b227856b4ab60037c93c6e15d6e54
MelihUnal60 <111892158+MelihUnal60@users.noreply.github.com>
2023-06-16 14:01:13 +0300

    General revisions

--- src/Components/MainContent/MainContent.js
+++ src/Components/MainContent/MainContent.js
@@ -8,30 +8,24 @@ import hosg from '../../Assets/hosg.jpg'
 
 function MainContent() {
     return (
-        <div className='container-fluid mt-5' style={{ height: '450px', backgroundColor: 'rgb(225, 250, 250)' }}>
-            <div className='row'>
-                <div className='col-12 col-sm-6'>
+        <div className={`container-fluid mt-5 ${styles.mainAna} `}>
+            <div className='row' >
+                <div className={`col-12 col-sm-6 ${styles.mainBox}`}>
 
-
-
+                    <h1 className={styles.mainText}>Çalışkan Dental'e Hoş Geldiniz!</h1>
+                    <h6 className={styles.mainText2}>34 yıllık tecrübemiz ve uzman kadromuz ile dişinizin sağlığı ve estetiği için yanınızdayız;</h6>
+                    <h6 className={styles.mainText2}><FaTooth /> Çalışkan Dent olarak sağlıkla ve güvenle gülümsemeniz için, kalıcı ve ekonomik çözümlerle hizmetinizdeyiz.</h6>
+                    <h6 className={styles.mainText2}><FaTooth /> Tedavi teknolojilerindeki gelişmeleri sürekli takip ederek en çağdaş malzemeler, en yeni cihazlar ile konforunuzu arttırmaya özen gösteriyoruz.</h6>
+                    <h6 className={styles.mainText2}><FaTooth /> Implantoloji, endodonti(kanal tedavisi), zirkonyum, çene cerrahisi, bleacthing(diş beyazlatma),kompozit-estetik dolgu uygulama ve operasyonlarında alanında uzman hekimlerimiz ile dişlerinizin sağlığına ve estetiğine katkı sunuyoruz.</h6>
+                </div>
+                <div className={`col-12 col-sm-6 ${styles.mainFoto}`}>
                     <img
-                        className='img-fluid'
                         src={hosg}
                         alt="Sağlıklı gülüşler"
                     />
                 </div>
-                <div className='col-12 col-sm-6'>
-
-                    <div className={styles.mainBox}>
-                        <h1 className={styles.mainText}>Çalışkan Dental'e Hoş Geldiniz!</h1>
-                        <h6 className={styles.mainText2}>34 yıllık tecrübemiz ve uzman kadromuz ile dişinizin sağlığı ve estetiği için yanınızdayız;</h6>
-                        <h6 className={styles.mainText2}><FaTooth /> Çalışkan Dent olarak sağlıkla ve güvenle gülümsemeniz için, kalıcı ve ekonomik çözümlerle hizmetinizdeyiz.</h6>
-                        <h6 className={styles.mainText2}><FaTooth /> Tedavi teknolojilerindeki gelişmeleri sürekli takip ederek en çağdaş malzemeler, en yeni cihazlar ile konforunuzu arttırmaya özen gösteriyoruz.</h6>
-                        <h6 className={styles.mainText2}><FaTooth /> Implantoloji, endodonti(kanal tedavisi), zirkonyum, çene cerrahisi, bleacthing(diş beyazlatma),kompozit-estetik dolgu uygulama ve operasyonlarında alanında uzman hekimlerimiz ile dişlerinizin sağlığına ve estetiğine katkı sunuyoruz.</h6>
-                    </div>
-
-                </div>
             </div>
+
         </div>
     )
 }
