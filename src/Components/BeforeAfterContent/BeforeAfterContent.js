93eb69a53897d5cfc5c2bb83eca2fb68b34e8174
MelihUnal60 <111892158+MelihUnal60@users.noreply.github.com>
2023-07-18 23:05:20 +0300

    BeforeAfter component files

---
+++ src/Components/BeforeAfterContent/BeforeAfterContent.js
@@ -0,0 +1,10 @@
+import styles from './BeforeAfterContent.module.scss';
+import React from 'react'
+
+function BeforeAfterContent() {
+  return (
+    <div>BeforeAfterContent</div>
+  )
+}
+
+export default BeforeAfterContent;
