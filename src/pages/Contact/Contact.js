a9c0b51a806f34d3f991c8e03964c5cf28e45e83
MelihUnal60 <111892158+MelihUnal60@users.noreply.github.com>
2023-07-18 20:42:22 +0300

    Revisions

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import styles from './Contact.module.scss';
+import ContactCard from '../../Components/ContactCard/ContactCard';
+
 
 const Contact=() => {
   return (
-    <div>Contact</div>
+    <div>
+      <ContactCard />
+      </div>
   )
 }
 
