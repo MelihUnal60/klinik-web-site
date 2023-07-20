43659712c20dc3b12d7cfdfcbf3d86ccf8a67dda
MelihUnal60 <111892158+MelihUnal60@users.noreply.github.com>
2023-07-20 10:04:12 +0300

    BeforeAfter works

--- src/Components/BeforeAfterContent/BeforeAfterContent.js
+++ src/Components/BeforeAfterContent/BeforeAfterContent.js
@@ -1,14 +1,26 @@
 import styles from './BeforeAfterContent.module.scss';
 import React from 'react';
 import baf1 from '../../Assets/baf1.png';
+import baf2 from '../../Assets/baf2.png';
+import baf4 from '../../Assets/baf4.png';
+import baf5 from '../../Assets/baf5.png';
 
 function BeforeAfterContent() {
   return (
     <div className={`container ${styles.beforeAfterContainer}`}>
-      <h2 className='text-center py-5'>Gülüşüne Estetik Kattıklarımız ;</h2>
+      <h2 className={`text-center py-5 ${styles.beforeAfterLabel}`}>Gülüşüne Estetik Kattıklarımız</h2>
       <div className='row'>
         <div className={`col-12 col-md-6 ${styles.afterPhoto}`}>
           <img src={baf1} />
+        </div>
+        <div className={`col-12 col-md-6 ${styles.afterPhoto}`}>
+          <img src={baf2} />
+        </div>
+        <div className={`col-12 col-md-6 py-5 ${styles.afterPhoto}`}>
+          <img src={baf4} />
+        </div>
+        <div className={`col-12 col-md-6 py-5 ${styles.afterPhoto}`}>
+          <img src={baf5} />
         </div>
         
       </div>
